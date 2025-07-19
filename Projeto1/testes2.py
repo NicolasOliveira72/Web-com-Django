e0b02b4c91f3a48fb634d45310922912cc69427e
nicolas <nicolas1421rocha@gmail.com>
2025-07-19 16:40:12 -0400

    :construction: Continuação de códigos em linguagem python

---
+++ Projeto1/testes2.py
@@ -0,0 +1,25 @@
+# Segunda parte dos códigos em linguagem python:
+
+# Operações com texto;
+
+texto = "Paralelepípido"
+print(texto[0])
+print(texto[2:8])
+print(texto[-4])
+print(texto[-6:-4])
+print(texto[::2])
+print()
+
+# Tratando conflito de aspas simples e duplas:
+
+frase = "Projeto em 'andamento'"
+print(frase)
+frase = 'Projeto em "andamento"'
+print(frase)
+frase = 'Projeto em \'andamento\''
+print(frase)
+frase = 'Projeto em' \
+' andamento'
+print(frase)
+print()
+
