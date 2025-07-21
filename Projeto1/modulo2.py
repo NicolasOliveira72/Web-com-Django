6463f0a27e78d7698fec2c283231756170e516e2
nicolas <nicolas1421rocha@gmail.com>
2025-07-21 09:35:55 -0400

    :construction: Códigos em linguagem python com a importação de módulos

---
+++ Projeto1/modulo2.py
@@ -0,0 +1,9 @@
+# Módulo para realizar importação para o principal:
+
+def somar(n1, n2):
+    return n1 + n2
+
+def multiplicar(num1, num2):
+    return num1 * num2
+
+idade = 30
---
+++ Projeto1/modulo_principal.py
@@ -0,0 +1,10 @@
+# Operações para serem utilizadas como módulos:
+
+from modulo2 import somar, idade
+from random import randint
+from math import sqrt
+
+print(somar(1,3))
+print(idade)
+print(randint(0,1000000))
+print(sqrt(400))
